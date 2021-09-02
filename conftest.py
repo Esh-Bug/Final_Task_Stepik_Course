dc23005f2e8385f11b8a738c0de3bc6637afa20a
Esh-Bug <84013140+Esh-Bug@users.noreply.github.com>
2021-09-02 23:18:42 +0300

    залив конфтеста

--- conftest.py
+++ conftest.py
@@ -1,20 +1,30 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="firefox",
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose browser: ru, en")
+
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Firefox()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
